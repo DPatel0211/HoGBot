991543e9736368ffac987303753015bbf46f8904
DPatel0211 <43239911+DPatel0211@users.noreply.github.com>
2019-12-10 12:41:56 -0500

    Update serverlist.js

--- commands/serverlist.js
+++ commands/serverlist.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-    return message.channel.send("Servers:")
     client.guilds.forEach((guild) => {
-        return message.channel.send(" - " + guild.name)
+        console.log(" - " + guild.name)
     })
 }
 
